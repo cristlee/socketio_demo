dcf3ce3eb9b714be6671afcf8ea151706ce23957
lifeng <cristlee@icloud.com>
2018-02-08 16:21:21 +0800

    add room concept

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, session
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
 import functools
 from model import User
 
@@ -9,6 +9,7 @@ socketio = SocketIO(app)
 
 
 users = 0
+buy_card_info = []
 room_info = []
 
 
@@ -75,8 +76,30 @@ def buy_card(message):
     cards = message['cards']
     u = session.get('u')
     u.buy_cards(cards)
-    room_info.append(u.name + ' buy ' + str(cards) + ' cards')
-    socketio.emit('room', room_info)
+    buy_card_info.append(u.name + ' buy ' + str(cards) + ' cards')
+    socketio.emit('buycard', buy_card_info)
+
+
+@socketio.on('join')
+@login_required
+def on_join(data):
+    u = session.get('u')
+    room = data['room']
+    join_room(room)
+    print rooms()
+    room_info.append(u.name + ' has entered the room.')
+    emit('room', room_info, room=room)
+
+
+@socketio.on('leave')
+@login_required
+def on_leave(data):
+    u = session.get('u')
+    room = data['room']
+    print rooms()
+    leave_room(room)
+    room_info.append(u.name + ' has left the room.')
+    emit('room', room_info, room=room)
 
 
 if __name__ == '__main__':
